b5ae4628724ff9a1e8aee6d601dd48845d047071
Noah Scala <nwscala@gmail.com>
2020-03-24 16:19:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(nameArray, giftReason) {
+    let giftArray = [];
+    
+    for (let i = 0; i < nameArray.length; i++) {
+        giftArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${giftReason} gift!`);
+    }
+
+    return giftArray;
+}
+
+function countDown(number) {
+    // let i = number;
+
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
