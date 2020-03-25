99ac037a7534ad62d71a6ff770ca4363c3706558
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-25 11:29:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+// writeCards()
+// ✓ returns an array of thank you messages for each name provided to the function
+
+// const friends = ["Lisa", "Kaitlin", "Jan"]
+
+function writeCards(people, occasion) {
+  let cardMessages = []
+  for (let i = 0; i < people.length; i++) {
+    cardMessages.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return cardMessages
+}
+
+// writeCards(friends, "surprise")
+
+// countDown()
+// 1) invokes console.log once for each number, counting down from the number provided to zero
+// 2) logs each number when counting down, starting from the number provided
+
+function countDown(number) {
+  let count = number
+  while (count >= 0) {
+    console.log(count--)
+  }
+}
+
