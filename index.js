1abc2ca1eafac6a8a55963532523ba3aa7510180
Zach Cullen <zacullen@gmail.com>
2020-04-08 12:20:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, str) {
+  let cards = []
+  for (let i = 0; i < arr.length; i++) {
+    cards.push(`Thank you, ${arr[i]}, for the wonderful ${str} gift!`);
+  }
+  return cards;
+}
+
+function countDown(int) {
+  while (int >= 0) {
+    console.log(int)
+    int--;
+  }
+}
