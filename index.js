564a335465d0b64284d54e94775c4620fb28bcbf
adrian mohabir <mohabiradrian@gmail.com>
2021-04-20 10:45:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-
-// const event = ["surprise"]
 function writeCards(names, event) {
 for (let i = 0; i < names.length; i++) {
     names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
 }
 return names;
 }
-writeCards(names);
+// writeCards(names);
 
-// function countDown(num) {
-//     let i = num; 
-//     while (i >= 0) {
-//         console.log(i); 
-//         i--;
-//     }
-// }
+
 function countDown(num) {
     while (num >= 0) {
         console.log(num--);
